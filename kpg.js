70c73b8b8d9130f46931f78ed4cdf0a811f9f742
Kirk Burleson <kirk.burleson@yahoo.com>
2016-05-01 12:34:55 -0500

    rewrite. handles char rules now

--- kpg.js
+++ kpg.js
@@ -1,48 +1,106 @@
-var DEFAULT_CHARS = 10,
-	MIN_CHARS = 6,
-	MAX_CHARS = 100;
+var DEFAULT_SIZE = 10,
+	MIN_SIZE = 8,
+	MAX_SIZE = 300,
+	LOWER = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+	UPPER = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+	NUMBERS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+	SPECIAL = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', '.', '?', '<', '>'],
+	ALL = LOWER.concat(UPPER, NUMBERS, SPECIAL);
 
-function kpg() {
-	var size = arguments[0];
+Array.prototype.shuffle = function() {
+	var i, lng, pos, tmp, floor, rand;
 
-	if (typeof size != 'number' || size < MIN_CHARS || size > MAX_CHARS)
-		size = DEFAULT_CHARS;
+	lng = this.length;
+	floor = Math.floor;
+	rand = Math.random;
 
-	return createPW(size);
-}
-
-function createPW(size) {
-	var i, pw = '';
-
-	for (i = 0; i < size; i++) {
-		chars = getCharPool();
-		pw += chars[Math.floor(Math.random() * (chars.length - 1))];
-	}
-
-	return pw;
-}
-
-function getCharPool() {
-	// returns shuffled pool of characters
-	var i, lng, result, pos,
-	pool = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-	'2', '3', '4', '5', '6', '7', '8', '9',
-	'!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?', '<', '>'];
-
-	lng = pool.length;
-	result = [];	
-	
 	// Fisher-Yates algorithm : Durstenfeld's version
-	for (i = 0; i < lng; ++i, --lng) {
-		pos = Math.floor(Math.random() * (lng - 1));
-		result.unshift(pool[pos]);
-		pool[pos] = pool[lng - 1];
+	for (i = 0; i < lng; i++, lng--) {
+		pos = floor(rand() * (lng - 1));
+		tmp = this[pos];
+		this[pos] = this[lng - 1];
+		this[lng-1] = tmp;
 	}
 
-	return result;
+	return this;
 }
 
-module.exports = {
-	get: kpg
-};
+Array.prototype.pick = function() {
+	return this[Math.floor(Math.random() * (this.length-1))];
+}
+
+function kpg(config) {
+	var i,
+		pw = [];
+
+	switch (typeof config) {
+		case 'function':
+		case 'string':
+		case 'undefined':
+		case 'boolean':
+		case 'null':
+			config = {};
+			break;
+		case 'number':
+			config = {sz:Math.floor(config)};
+			break;
+		default:
+			config = config || {};
+	}
+
+	config.l = typeof config.l == 'number' ? Math.floor(config.l) : 0;
+	config.u = typeof config.u == 'number' ? Math.floor(config.u) : 0;
+	config.n = typeof config.n == 'number' ? Math.floor(config.n) : 0;
+	config.s = typeof config.s == 'number' ? Math.floor(config.s) : 0;
+	config.sz = typeof config.sz == 'number' ? Math.floor(config.sz) : DEFAULT_SIZE;
+
+	// enforcements are greater than size
+	if (config.l + config.u + config.n + config.s > config.sz)
+		config.sz = (config.l + config.u + config.n + config.s);
+
+	if (config.sz < MIN_SIZE || config.sz > MAX_SIZE)
+		config.sz = DEFAULT_SIZE;
+
+	
+
+	// lower case
+	for (i = 0; i < config.l; i++) {
+		LOWER.shuffle();
+		pw.unshift(LOWER.pick());
+	}
+	config.sz -= config.l;
+
+	// upper case
+	for (i = 0; i < config.u; i++) {
+		UPPER.shuffle();
+		pw.unshift(UPPER.pick());
+	}
+	config.sz -= config.u;
+
+	// numbers
+	for (i = 0; i < config.n; i++) {
+		NUMBERS.shuffle();
+		pw.unshift(NUMBERS.pick());
+	}
+	config.sz -= config.n;
+
+	// special characters
+	for (i = 0; i < config.s; i++) {
+		SPECIAL.shuffle();
+		pw.unshift(SPECIAL.pick());
+	}
+	config.sz -= config.s;
+
+	// the rest
+	if (config.sz > 0) {
+		for (i = config.sz; i > 0; i--) {
+			ALL.shuffle();
+			pw.unshift(ALL.pick());
+		}
+	}
+
+
+	return pw.shuffle().join('');
+}
+
+module.exports = kpg;
