ee12e552ca63e54a3911817bdaf92cd6c0c08356
Kirk Burleson <kirk.burleson@yahoo.com>
2016-05-06 14:20:35 -0500

    v3 changes. see readme

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 OVERVIEW
 ========
 
-kpg is used for generating passwords. The passwords can be 8 to 300 characters long and you can enforce minimums for lower case, upper case, numbers, and special characters.
+kpg is used for generating passwords. The passwords must be at least 1 character long, and you can enforce minimums for lower case, upper case, numbers, and special characters.
 
 INSTALLATION
 ============
@@ -20,12 +20,12 @@ Now you can start generating passwords. If you don't pass a config object, kpg g
 // sample: 7t0@{NbJvC<br/>
 var pw = kpg();
 
-That was simple! Now let's play with the config object. Let's make a 12 character password that's all lower case: Add the 'l' property for lower.<br/>
+That was simple! Now let's play with the config object. Let's make a 12 character password that's all lower case: Add the 'l' property for lower and the 'sz' property for the size in characters.<br/>
 
 // sample: kjzfzenmiild<br/>
 var pw = kpg( {sz:12, l:12} );
 
-Let's make a 15 character password that's all upper case: Add the 'u' property for upper.
+Let's make a 15 character password that's all upper case: Add the 'u' property for upper. NOTE: We don't have to set the size if the enforcement properties are at least the default size. The size will grow to accomodate the properties.
 
 // sample: LTXCFQCJTQWNPBS<br/>
 var pw = kpg( {sz:15, u:15} );
@@ -51,3 +51,18 @@ One more. 50 characters with at least 49 numbers and 1 upper case letter.
 var pw = kpg( {sz:50, n:49, u:1} );
 
 That's all there is to it! The properties can be in any order and if the number of the properties exceeds the 'sz' property, the size will grow to their sum. If you give 'sz:12' and 'n:11' and 'l:10', you will get a password with 21 characters.
+
+If you're wondering about a maximum length, I've tested with a size of 10 million and it scaled at 1 second per million on an i7 3930k running Windows 8.0 and 16gb of ram, using nodeJS. Linux Mint 17.2, running in a virtualbox, took maybe a second more.
+
+CHANGES:
+========
+
+V.3:
+Minimum size changed from 8 to 1
+Maximum size removed
+Significantly faster at sizes of 100k+
+Supports size passed as a string. // kpg("20"), instead of kpg(20);
+
+V.2:
+Introduced passing a config object in javascript
+Introduced minimums for numbers, special chars, lower case, and upper case chars
--- kpg.js
+++ kpg.js
@@ -1,18 +1,17 @@
 var DEFAULT_SIZE = 10,
-	MIN_SIZE = 8,
-	MAX_SIZE = 300,
+	MIN_SIZE = 1,
 	LOWER = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
 	UPPER = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
 	NUMBERS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
 	SPECIAL = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', '.', '?', '<', '>'],
-	ALL = LOWER.concat(UPPER, NUMBERS, SPECIAL);
+	ALL = LOWER.concat(UPPER, NUMBERS, SPECIAL)
+	floor = Math.floor,
+	rand = Math.random;
 
 function shuffle(arr) {
-	var i, lng, pos, tmp, floor, rand;
+	var i, lng, pos, tmp;
 
 	lng = arr.length;
-	floor = Math.floor;
-	rand = Math.random;
 
 	// Fisher-Yates algorithm : Durstenfeld's version
 	for (i = 0; i < lng; i++, lng--) {
@@ -25,82 +24,110 @@ function shuffle(arr) {
 	return arr;
 }
 
-function pick(arr) {
-	return arr[Math.floor(Math.random() * (arr.length-1))];
-}
-
 function kpg(config) {
-	var i,
-		pw = [];
+	var i, j, t, pw, cnt, lng, tmp, pos;
 
-	switch (typeof config) {
-		case 'function':
-		case 'string':
-		case 'undefined':
-		case 'boolean':
-		case 'null':
-			config = {};
-			break;
-		case 'number':
-			config = {sz:Math.floor(config)};
-			break;
-		default:
-			config = config || {};
-	}
+	t = typeof config;
 
-	config.l = typeof config.l == 'number' ? Math.floor(config.l) : 0;
-	config.u = typeof config.u == 'number' ? Math.floor(config.u) : 0;
-	config.n = typeof config.n == 'number' ? Math.floor(config.n) : 0;
-	config.s = typeof config.s == 'number' ? Math.floor(config.s) : 0;
-	config.sz = typeof config.sz == 'number' ? Math.floor(config.sz) : DEFAULT_SIZE;
+	if (t == "number")
+		config = {sz:config || {}}; // catches NaN
 
-	// enforcements are greater than size
+	else if (t == "string")
+		config = {sz:+config || {}}; // catches NaN
+
+	else if (t == "function" || t == "undefined" || t == "boolean" || t == "null")
+		config = {};
+
+	else
+		config = config || {};
+
+	config.l = typeof config.l == 'number' ? floor(config.l) : 0;
+	config.u = typeof config.u == 'number' ? floor(config.u) : 0;
+	config.n = typeof config.n == 'number' ? floor(config.n) : 0;
+	config.s = typeof config.s == 'number' ? floor(config.s) : 0;
+	config.sz = typeof config.sz == 'number' ? floor(config.sz) : DEFAULT_SIZE;
+
+	// size grows to property sum
 	if (config.l + config.u + config.n + config.s > config.sz)
 		config.sz = (config.l + config.u + config.n + config.s);
 
-	if (config.sz < MIN_SIZE || config.sz > MAX_SIZE)
+	if (config.sz < MIN_SIZE)
 		config.sz = DEFAULT_SIZE;
 
-	
+	pw = Array(config.sz);
+	cnt = 0;
 
 	// lower case
 	for (i = 0; i < config.l; i++) {
-		shuffle(LOWER);
-		pw.unshift(pick(LOWER));
+		for (j = 0, lng = LOWER.length; j < lng; j++, lng--) {
+			pos = floor(rand() * (lng-1));
+			tmp = LOWER[pos];
+			LOWER[pos] = LOWER[lng-1];
+			LOWER[lng-1] = tmp;
+		}
+		pw[cnt++] = LOWER[floor(rand() * (LOWER.length-1))];
 	}
 	config.sz -= config.l;
 
 	// upper case
 	for (i = 0; i < config.u; i++) {
-		shuffle(UPPER);
-		pw.unshift(pick(UPPER));
+		for (j = 0, lng = UPPER.length; j < lng; j++, lng--) {
+			pos = floor(rand() * (lng-1));
+			tmp = UPPER[pos];
+			UPPER[pos] = UPPER[lng-1];
+			UPPER[lng-1] = tmp;
+		}
+		pw[cnt++] = UPPER[floor(rand() * (UPPER.length-1))];
 	}
 	config.sz -= config.u;
 
 	// numbers
 	for (i = 0; i < config.n; i++) {
-		shuffle(NUMBERS);
-		pw.unshift(pick(NUMBERS));
+		for (j = 0, lng = NUMBERS.length; j < lng; j++, lng--) {
+			pos = floor(rand() * (lng-1));
+			tmp = NUMBERS[pos];
+			NUMBERS[pos] = NUMBERS[lng-1];
+			NUMBERS[lng-1] = tmp;
+		}
+		pw[cnt++] = NUMBERS[floor(rand() * (NUMBERS.length-1))];
 	}
 	config.sz -= config.n;
 
 	// special characters
 	for (i = 0; i < config.s; i++) {
-		shuffle(SPECIAL);
-		pw.unshift(pick(SPECIAL));
+		for (j = 0, lng = SPECIAL.length; j < lng; j++, lng--) {
+			pos = floor(rand() * (lng-1));
+			tmp = SPECIAL[pos];
+			SPECIAL[pos] = SPECIAL[lng-1];
+			SPECIAL[lng-1] = tmp;
+		}
+		pw[cnt++] = SPECIAL[floor(rand() * (SPECIAL.length-1))];
 	}
 	config.sz -= config.s;
 
 	// the rest
 	if (config.sz > 0) {
 		for (i = config.sz; i > 0; i--) {
-			shuffle(ALL);
-			pw.unshift(pick(ALL));
+			for (j = 0, lng = ALL.length; j < lng; j++, lng--) {
+				pos = floor(rand() * (lng-1));
+				tmp = ALL[pos];
+				ALL[pos] = ALL[lng-1];
+				ALL[lng-1] = tmp;
+			}
+			pw[cnt++] = ALL[floor(rand() * (ALL.length-1))];
 		}
 	}
 
+	// final shuffle
+	for (i = 0, lng = pw.length; i < lng; i++, lng--) {
+		pos = floor(rand() * (lng-1));
+		tmp = pw[pos];
+		pw[pos] = pw[lng-1];
+		pw[lng-1] = tmp;
+	}
 
-	return shuffle(pw).join('');
+
+	return pw.join('');
 }
 
 module.exports = kpg;
